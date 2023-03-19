7f92fb9dd5faf3851cbf81357c26d52627bbe9e4
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2023-03-20 02:33:57 +0800

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ tpmodul5_1302210113/HaloGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302210113
+{
+    public class HaloGeneric
+    {
+        public static void sapaUser<T>(T X)
+        {
+            Console.WriteLine("Hallo user " + X);
+        }
+
+    }
+}
--- tpmodul5_1302210113/Program.cs
+++ tpmodul5_1302210113/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+using tpmodul5_1302210113;
+
+
+class tpmodul5_130221010113
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric.sapaUser<string>("Yakup Asmaidy Atanggae");
+        //DataGeneric<string> nim = new DataGeneric<string>("1302210113");
+        //nim.PrintData();
+    }
+}
